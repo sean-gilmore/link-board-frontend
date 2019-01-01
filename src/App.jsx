f2c577c53f2090bd81e944bc70006c82da2249fd
Sean <gilmore.seven@gmail.com>
2019-01-01 23:12:54 +1100

    Get flow, scss and eslint all working together

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,9 @@
 import * as React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './assets/styles/index.scss';
+import Root from './pages/Root.jsx';
 
 const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit
-        {' '}
-        <code>src/App.js</code>
-        {' '}
-        test
-        and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>
+  <Root/>
 );
 
 export default App;
---
+++ src/pages/Root.jsx
@@ -0,0 +1,16 @@
+// @flow
+import * as React from 'react';
+
+type Props = {
+
+};
+
+class Root extends React.Component<Props> {
+  render () {
+    return ( 
+      <span>Test</span>
+    );
+  }
+}
+
+export default Root;
