91b7d891d8f6b18c17d5d252e044475485a9dc25
Sean <gilmore.seven@gmail.com>
2019-01-05 13:11:48 +1100

    create navigation component

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,21 @@
+// @flow
+import * as React from 'react';
+
+type Props = {
+
+};
+
+class Navigation extends React.Component<Props> {
+  render() {
+    return (
+      <nav className='navigation'>
+        <h3>My Lists</h3>
+        <ul>
+          <li></li>
+        </ul>
+      </nav>
+    )
+  }
+}
+
+export default Navigation;
