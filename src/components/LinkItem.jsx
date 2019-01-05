37076237f5dda2f8553b9d19e258b495c85e90a4
Sean <gilmore.seven@gmail.com>
2019-01-05 13:11:27 +1100

    display only link

--- src/components/LinkItem.jsx
+++ src/components/LinkItem.jsx
@@ -2,16 +2,14 @@
 import * as React from 'react';
 
 type Props = {
-  title: string,
   link: string
 };
 
 class LinkItem extends React.Component<Props> {
   render() {
-    const { title, link } = this.props;
+    const { link } = this.props;
     return (
       <li className='link-item'>
-        {title}
         {link}
       </li>
     )
