51e27f63d264cb6b8903764b4fd6d0c5193a63b0
Sean <gilmore.seven@gmail.com>
2019-02-24 22:50:45 +1100

    create a dummy create-list component
    - This component will handle logic for creating lists

---
+++ src/components/CreateList.js
@@ -0,0 +1,16 @@
+// @flow
+import * as React from 'react';
+
+type Props = {
+
+};
+
+class CreateList extends React.Component<Props> {
+  render() {
+    return (
+      <span>Test</span>
+    )
+  }
+}
+
+export default CreateList;
