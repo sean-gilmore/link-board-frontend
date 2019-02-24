630948321df506a96eb8ebefec2b728f679d68c1
Sean <gilmore.seven@gmail.com>
2019-02-24 22:50:21 +1100

    add react router, routerfy Root container

--- src/containers/Root.jsx
+++ src/containers/Root.jsx
@@ -1,9 +1,11 @@
 // @flow
 import * as React from 'react';
 import LinkList from '../components/LinkList';
+import CreateList from '../components/CreateList';
 import Navigation from '../components/Navigation';
 import { Provider } from 'react-redux'
 import configureStore from '../configureStore'
+import { BrowserRouter as Router, Route} from 'react-router-dom';
 
 const store = configureStore()
 
@@ -17,7 +19,12 @@ class Root extends React.Component<Props> {
       <Provider store={store}>
         <div className='root'>
           <Navigation />
-          <LinkList />
+          <Router>
+            <div>
+              <Route path="/create-list" component={CreateList} />
+              <Route path="/list/{listId}" component={LinkList} />
+            </div>
+          </Router>
         </div>
       </Provider>
     );
